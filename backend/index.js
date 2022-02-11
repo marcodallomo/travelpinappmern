6f09d00d2b1cb1e057b9ec5c064d3ef0f47b24c1
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2022-02-11 09:29:17 +0000

    latest

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
 
-app.listen(process.env.PORT || 8800, () => {
+app.listen(process.env.PORT, () => {
   console.log("backend server is running");
 });
